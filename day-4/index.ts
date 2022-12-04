27188366a16387c06a9cb1b1e9fbdc0beb031256
robbryandev <robbryandev@gmail.com>
2022-12-04 11:58:51 -0800

    day-4 part 2

--- day-4/index.ts
+++ day-4/index.ts
@@ -2,28 +2,45 @@ import * as fs from "fs";
 
 const input: string[] = fs.readFileSync("./data/input.txt", "utf-8").split("\n");
 
-function isConflict(groupOne: string, groupTwo: string): boolean {
+function isConflict(groupOne: string, groupTwo: string): [boolean, boolean] {
   const splitOne: string[] = groupOne.split("-");
   const splitTwo: string[] = groupTwo.split("-");
   const valOne: [number, number] = [parseInt(splitOne[0]), parseInt(splitOne[1])];
   const valTwo: [number, number] = [parseInt(splitTwo[0]), parseInt(splitTwo[1])];
 
-  if (valTwo[0] >= valOne[0]) {
-    return valTwo[1] <= valOne[1];
-  } else {
-    return false;
+  let groupOneRange: number[] = [];
+  for (let i = valOne[0]; i <= valOne[1]; i++) {
+    groupOneRange.push(i);
   }
+
+  let isIn: boolean = false;
+  let fullIn: boolean = false;
+
+  if (groupOneRange.includes(valTwo[0]) || groupOneRange.includes(valTwo[1])) {
+    isIn = true;
+  }
+  if (valTwo[0] >= valOne[0]) {
+    fullIn = valTwo[1] <= valOne[1];
+  }
+  return [fullIn, isIn];
 }
 
 let conflicts: number = 0;
+let fullConflicts: number = 0;
 for (let i in input) {
   const groups: string[] = input[i].split(",");
   const teamOne: string = groups[0];
   const teamTwo: string = groups[1];
 
-  if (isConflict(teamOne, teamTwo) || isConflict(teamTwo, teamOne)) {
+  const conOne = isConflict(teamOne, teamTwo);
+  const conTwo = isConflict(teamTwo, teamOne);
+  if (conOne[0] || conTwo[0]) {
+    fullConflicts++;
+  }
+  if (conOne[0] || conOne[1] || conTwo[0] || conTwo[1]) {
     conflicts++;
   }
 }
 
+console.log(fullConflicts);
 console.log(conflicts);
